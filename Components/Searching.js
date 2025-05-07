c7cb0e5885c0f1d71766e2cbd6aac3931a486be5
pranavavadivel <pranavavadivel01@example.com>
2025-05-08 01:19:41 +0530

    Added Gemini API

--- Components/Searching.js
+++ Components/Searching.js
@@ -1,11 +1,16 @@
-const { isURL, DBSearching } = require("../DB/utils");
+const { isURL, DBSearching, GeminiSearch } = require("../DB/utils");
 const { scrapeWebsite } = require("./scrapping");
 
 const searching = async (data) => {
     if (isURL(data)) {
         return await scrapeWebsite(data) // if paras is URL it will scrap the data
+    }
+
+    let DbSearch = await DBSearching(data) // if the url is word .it will search and return the data from DB
+    if (DbSearch.length == 0) {
+        return await GeminiSearch(data)
     } else {
-        return await DBSearching(data) // if the url is word .it will search and return the data from DB
+        return DbSearch
     }
 }
 
--- DB/utils.js
+++ DB/utils.js
@@ -1,3 +1,4 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const { insertOneClient, updateClient, getDataFromDB } = require("./mongoDB");
 
 const storingNewURldata = (data) => { // storing new data in DB
@@ -44,5 +45,31 @@ const DBSearching = async (word) => { // searhcing the relevent keyword in DB
     return searchedDatas;
 };
 
+const GeminiSearch = async (trasulationCount) => { // gemini search for extra usage
+    console.log('come to gemini');
 
-module.exports = { storingNewURldata, patchTheStoringURL, isURL, DBSearching }
+    try {
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_TEXTENHANCER_KEY)
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+        // const prompt = `Translate this to ${value} .Give the responce in this language only and send the statement only ,without "\n". Use Google Translate for translation. The statement is: "${req.body.feedingValue}"`;
+        const paras = `Give minimum 20 paras of detail about ${trasulationCount}.Give the responce in this English language only and send the statement only,without "\n",use Search.`
+        const getURL = `Use Search and give a URL for ${trasulationCount} for studying about this.i need URL only.not any text or sentence`
+
+        const result = {}
+        result['paras'] = (await model?.generateContent(paras)).response.text().replace(/[\r\n]+/g, '');
+        result['URL'] = (await model?.generateContent(getURL)).response.text().replace(/[\r\n]+/g, '');
+
+        if (!result) {
+            throw new Error('Model Failed....☹');
+        }
+        
+        const sentences = result?.paras.split(/\.\s*/).filter(Boolean); // Remove empty strings
+        result['paras'] = sentences
+
+        return result
+    } catch (err) {
+        return err.message
+    }
+}
+
+module.exports = { storingNewURldata, patchTheStoringURL, isURL, DBSearching, GeminiSearch }
